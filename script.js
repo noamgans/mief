43a1272de270978a3c0a6191c715435300c32a09
Noam Gans <noamgans@Noams-MacBook-Air.local>
2024-08-16 15:24:59 -0400

    subheadings and styling

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function loadHomePage() {
         content.innerHTML = `
-            <section>
-                <h1>Welcome to the Minutemen International Equity Fund</h1>
-                <p>Our club is dedicated to understanding and investing in international equities.</p>
-            </section>
+        <h1>Welcome to the Minutemen International Equity Fund</h1>
+        <div class="container">
+            <div class="left-half">
+                <h3>Purpose</h3>
+                <p>Our purpose is to connect remote and on-campus students who share an enthusiasm for international equity research - financial analysis in international markets.</p>
+                <h3>Goals</h3>
+            <p>We seek to educate and provide hands-on experience to our analysts allowing them to develop highly valuable analytical skills that can translate into future careers in the financial services industry.</p>
+            
+                
+            </div>
+            <div class="right-half">
+            <h3>Strategy</h3>
+                <p>Through using valuations and reports from our analysts, we develop our long/short active trading strategy to create alpha for our club's portfolio.</p>
+            <h3>Philosophy</h3>
+                <p>At MIEF, we believe in a comprehensive approach to international equity research. Be analytical, logical, and quantitative, always acting upon due dilligence and allowing yourself to learn from mistakes We embrace learning from both positive and negative experiences, as we view them as an opportunity to expand our knowledge and reduce our future risk.</p>
+            </div>
+        </div>
         `;
     }
 
